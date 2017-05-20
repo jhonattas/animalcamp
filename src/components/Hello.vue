cb8123d188797e76564d8c9baa4962140e81077f
Jhonattas <jhonattas.goncalves@gmail.com>
2017-05-20 11:52:43 -0300

    cria uma pagina temporaria para derrubar a anterior

---
+++ src/components/Hello.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <p>
+    <b>No momento estamos em manutenção</b><br/>
+    Mas você ainda pode falar com a gente, ou encontrar mais informações sobre nós no <a href="https://fb.com/animalcamppetshop">facebook.com/AnimalCampPetshop</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+p {
+  font-size: 1.5em;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
